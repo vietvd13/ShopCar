2d931e60c0b4f447d972b16908003717144739c2
Duck <vuducviet0131@gmail.com>
2022-11-17 09:59:40 +0700

    Update overwire variables style bs

--- src/main.js
+++ src/main.js
@@ -18,8 +18,6 @@ import '@/scss/index.scss';
 
 // Bootstrap Vue
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
