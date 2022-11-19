3dbc6dc7f5b0e2d164e8c3d5cb25359afad6fc9a
Duck <vuducviet0131@gmail.com>
2022-11-19 10:58:04 +0700

    View car buying policy

--- src/views/ShopCar/CarBuyingPolicy.vue
+++ src/views/ShopCar/CarBuyingPolicy.vue
@@ -7,18 +7,24 @@
         </div>
 
         <b-card>
-        
+            <VuePdfApp
+                style="height: 100vh;"
+                pdf="https://api-cars.thangld-dev.tech/uploads/uploads-1668829787061-239f0249-5114-41b8-831a-96129883348b.pdf"
+            />
         </b-card>
     </div>
 </template>
 
 <script>
 import TitleContent from './components/TitleContent.vue';
+import VuePdfApp from "vue-pdf-app";
+import "vue-pdf-app/dist/icons/main.css";
 
 export default {
     name: 'CarBuyingPolicy',
     components: {
         TitleContent,
+        VuePdfApp
     },
 }
 </script>
