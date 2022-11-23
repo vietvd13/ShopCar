bf1b2c5b2547ebbd788267621ad45e0e30e4d0fd
Duck <vuducviet0131@gmail.com>
2022-11-23 13:18:05 +0700

    Update lang

--- src/langs/subs/en.js
+++ src/langs/subs/en.js
@@ -1,48 +1,272 @@
 export default {
   APP: {
-    LOADING: 'LOADING'
+    LOADING: "Loading...",
+    BUTTON_BACK: "Come back",
+    BUTTON_ADD: "Add new",
+    BUTTON_DELETE_MANY: "Delete a lot",
+    BUTTON_UPDATE: "Update",
+    BUTTON_FILTER: "Apply",
+    TABLE_NO_DATA: "No data",
+    PLEASE_IMPORT_IMAGE: "Photo selection",
+    REQUIRED_IMAGE_SIZE_4_6: "Size 4x6",
+    DELETE: "Delete",
+    CANCEL: "Cancel",
+    SAVE: "Save",
+    UPDATE_STATUS_DONE: "Done processing",
+    UPDATE_STATUS_IN_PROGRESS: "No process",
+    TEXT_PER_PAGE_10: "ten",
+    TEXT_PER_PAGE_20: "20",
+    TEXT_PER_PAGE_40: "40",
+    TEXT_PER_PAGE_80: "80",
+    TEXT_PER_PAGE_100: "100",
+    IMPORT_IMAGE: "Photo selection",
+    PLEASE_INPUT: "Please enter",
+    PLEASE_SELECT: "Please choose",
+    TEXT_VIEW_MORE: "See all...",
+    PREVIEW: "Preview",
+    UPDATE_BANNER: "Update banner",
+    NO_DATA: "No data",
+    HOT_SALE: "Hot Sale"
   },
   TOAST: {
-    SUCCESS: 'SUCCESS',
-    WARNING: 'WARNING',
-    DANGER: 'DANGER',
+    SUCCESS: "Successful",
+    WARNING: "Warning",
+    DANGER: "Error"
+  },
+  LANGUAGES: {
+    LANGUAGES: "Language",
+    VIETNAMESE: "Vietnamese",
+    ENGLISH: "English",
+    KOREAN: "Korean"
+  },
+  ROUTER: {
+    COLLABORATORS_MANAGEMENT: "Collaborators",
+    CAR_MANAGEMENT: "Vehicle management",
+    HAPPY_MOMENT: "Happy moment",
+    CUSTOMER_SUPPORT: "Customer support",
+    BANNER_MANAGEMENT: "Manage banners",
+    PROFILE: "Personal information",
+    POLICY_MANAGEMENT: "Policy management",
+    INSURANCE_MANAGEMENT: "Insurance management"
+  },
+  TOAST_MESSAGE: {
+    CHANGE_LANGUAGE_SUCCESS: "You have successfully changed the language",
+    CHANGE_LANGUAGE_ERROR: "You have failed to change the language",
+    VALIDATE_ACCOUNT_LOGIN: "The account format is incorrect. Please check your account again",
+    LOGIN_ERROR: "Login failed, please check your account again",
+    LOGOUT_ERROR: "Logout failed",
+    CREATE_COLLABORATORS_SUCCESS: "You have successfully created a collaborator",
+    CREATE_COLLABORATORS_ERROR: "You have failed to create a collaborator",
+    EDIT_COLLABORATORS_SUCCESS: "You have successfully edited the collaborator information",
+    EDIT_COLLABORATORS_ERROR: "You have failed to edit the collaborator information",
+    DELETE_COLLABORATORS_SUCCESS: "You have successfully deleted the collaborator",
+    DELETE_COLLABORATORS_ERROR: "You deleted the failed collaborator",
+    CREATE_HAPPY_MOMENT_SUCCESS: "You have successfully created a fun moment",
+    CREATE_HAPPY_MOMENT_ERROR: "You created a failed happy moment",
+    EDIT_HAPPY_MOMENT_SUCCESS: "You have successfully edited the happy moment",
+    EDIT_HAPPY_MOMENT_ERROR: "You edited the failed happy moment",
+    DELETE_HAPPY_MOMENT_SUCCESS: "You have successfully erased the happy moment",
+    DELETE_HAPPY_MOMENT_ERROR: "You erased the happy failed moment",
+    EDIT_CUSTOMER_SUPPORT_SUCCESS: "You have successfully updated your status",
+    EDIT_CUSTOMER_SUPPORT_ERROR: "Your status update failed",
+    CREATE_CUSTOMER_SUPPORT_SUCCESS: "You have successfully created a support contact. Please wait for our response",
+    CREATE_CUSTOMER_SUPPORT_ERROR: "You have not successfully created support contact, please try again",
+    UPDATE_BANNER_SUCCESS: "You have successfully updated the banner",
+    UPDATE_BANNER_ERROR: "You have failed to update the banner",
+    REQUIRED_FILE: "Please select file",
+    CHANGE_PROFILE_SUCCESS: "You have successfully updated your profile",
+    CHANGE_PROFILE_ERROR: "You have failed to update your profile"
   },
   SHOP_CAR: {
     HOME: {
-      SEARCH: 'SEARCH',
+      SEARCH: "Search",
       CATEGORIES: {
-        DOMESTIC_CAR: 'Domestic car',
-        IMPORTED_CAR: 'Imported car',
-        CUSTOMER_REVIEWS: 'Customer reviews',
-        VEHICLE_LIST: 'Vehicle List'
+        VEHICLE_LIST: "Vehicle List",
+        CAR_BUYING_POLICY: "Car buying policy",
+        INSURANCE: "Insurance",
+        CONTACT: "Contact"
+      },
+      FILTER: {
+        YEAR: "Year of manufacture",
+        CATEGORIES: "Car company",
+        COLOR: "Color",
+        FUEL_TYPE: "Fuel type",
+        GEAR_BOX: "Engine",
+        DISTANCE: "Distance",
+        PRICE: "Price"
       },
       SORT_LIST_CAR: {
-        CAR_NAME: 'CAR_NAME',
-        PRICE: 'PRICE',
-        YEAR_MANUFACTURE: 'YEAR_MANUFACTURE',
-
-        TEXT_PER_PAGE_8: 'TEXT_PER_PAGE_8',
-        TEXT_PER_PAGE_16: 'TEXT_PER_PAGE_16',
-        TEXT_PER_PAGE_24: 'TEXT_PER_PAGE_24',
-        TEXT_PER_PAGE_32: 'TEXT_PER_PAGE_32',
-        TEXT_PER_PAGE_40: 'TEXT_PER_PAGE_40',
+        CAR_NAME: "Vehicle name",
+        PRICE: "Price",
+        YEAR_MANUFACTURE: "Year of manufacture",
+        TEXT_PER_PAGE_8: "8 cars/page",
+        TEXT_PER_PAGE_16: "16 cars/page",
+        TEXT_PER_PAGE_24: "24 cars/page",
+        TEXT_PER_PAGE_32: "32 cars/page",
+        TEXT_PER_PAGE_40: "40 cars/page"
       },
       NEW_CAR: {
-        TITLE: 'TITLE',
+        TITLE: "Top new cars"
       },
       HOT_SALE: {
-        TITLE: 'TITLE',
+        TITLE: "Hotsale"
       },
       CUSTOMER_FEEDBACK: {
-        TITLE: 'TITLE'
+        TITLE: "Happy moment",
+        DETAIL_TITLE: "Fun moment details"
+      },
+      COLLABORATORS: {
+        TITLE: "Collaborators"
+      },
+      CONTACT_SUPPORT: {
+        TITLE: "Contact - Q&A",
+        NAME: "Name",
+        PHONE: "Phone number",
+        CONTENT: "Content",
+        SEND: "To send"
+      },
+      CONTACT: {
+        PHONE: "Phone number",
+        ADDRESS: "Address",
+        EMAIL: "Email",
+        MAP: "Map"
+      },
+      PROFILE: {
+        TITLE: "Personal page"
+      },
+      LOGOUT: {
+        TITLE: "Log out"
       }
     },
     DETAIL_CAR: {
-      TITLE_BASIC_INFOR: 'TITLE_BASIC_INFOR',
-      YEAR: 'YEAR',
-      FUEL_TYPE: 'FUEL_TYPE',
-      COLOR: 'COLOR',
-      CAR_NUMBER: 'CAR_NUMBER',
+      TITLE: "Detailed vehicle information",
+      TITLE_IMAGE: "Image information",
+      TITLE_BASIC_INFOR: "Basic information",
+      TITLE_SALLER_INFOR: "Seller information",
+      TITLE_VEHICLE_OPTIONS_AND_DETAILS: "Vehicle options and details",
+      TITLE_PERFORMANCE_CHECK: "Performance test",
+      PRICE: "Price",
+      LICENSE_PLATE: "License plates",
+      YEAR: "Five",
+      DISTANCE_DRIVEN: "Driving distance",
+      FUEL_TYPE: "Fuel",
+      TRANSMISSION: "Transmission",
+      DISPLACEMENT: "Move",
+      COLOR: "Color",
+      CAR_TYPE: "Range of vehicle",
+      SEIZURE: "Convulsions",
+      MORTGAGE: "Mortgage",
+      PRESENTATION_NUMBER: "Presentation number",
+      STORAGE_LOCATION: "Storage area",
+      CONTACT: "Contact",
+      NAME: "Name",
+      EMPLOYEE_ID: "Employee ID",
+      AFFILIATED_COMPANY: "Under",
+      BUSINESS_ADDRESS: "Business address",
+      PARKING_LOCATION: "Parking location",
+      CAR_OPTION_EXTERIOR: "Exterior",
+      CAR_OPTION_GUTS: "Interior",
+      CAR_OPTION_SAFETY: "Safe",
+      CAR_OPTION_CONVENIENCE: "Utilities"
+    },
+    CAR_BUYING_POLICY: {
+      TITLE: "Car buying policy"
+    },
+    CONTACT: {
+      TITLE: "Contact"
+    }
+  },
+  DASHBOARD: {
+    LOGIN: {
+      TITLE: "Log in",
+      EMAIL: "Email",
+      PASSWORD: "Password",
+      LOGIN: "Log in"
+    },
+    COLLABORATORS_MANAGEMENT: {
+      SEARCH: "Search",
+      TABLE: {
+        NO: "STT",
+        AVATAR: "Avatar",
+        FULLNAME: "First and last name",
+        TELEPHONE: "Phone number",
+        EMAIL: "Email",
+        CONTACT: "Contact",
+        ACTION: "Manipulation",
+        SNS_KAKAOTALK: "KakaoTalk",
+        SNS_ZALO: "Zalo",
+        SNS_MESSAGER: "Messager",
+        DESCRIPTION: "Describe",
+        EDIT: "Edit",
+        DELETE: "Delete"
+      },
+      MODAL: {
+        TITLE: "Manage collaborators"
+      }
+    },
+    HAPPY_MOMENT: {
+      SEARCH: "Search",
+      TABLE: {
+        NO: "STT",
+        PRIMARY_IMAGE: "Image",
+        TITLE: "Title",
+        CONTENT: "Content",
+        WRITER: "Creator",
+        ACTIONS: "Manipulation",
+        BTN_EDIT: "Edit",
+        BTN_DELETE: "Delete"
+      },
+      MODAL: {
+        TITLE: "Manage fun moments",
+        LABEL_TITLE: "Title",
+        LABEL_CONTENT: "Content",
+        LABEL_IMAGES: "Picture"
+      }
+    },
+    CUSTOMER_SUPPORT: {
+      TABLE: {
+        NO: "STT",
+        NAME: "First and last name",
+        PHONE: "Phone number",
+        QUESTION: "Question",
+        STATUS: "Processing status",
+        ACTION: "Manipulation",
+        DETAIL: "Display",
+        IN_PROGRESS: "No process",
+        DONE: "Complete"
+      }
+    },
+    PROFILE: {
+      INFOR: "Basic information",
+      CHANGEPASSWORD: "Change Password",
+      OLDPASSWORDS: "old password",
+      NEWPASSWORDS: "A new password",
+      FULLNAME: "First and last name",
+      DOB: "Date of birth",
+      EMAIL: "Email address",
+      PHONE: "Phone number",
+      ADDRESS: "Address",
+      ADDRESS_MAIL_NOTI: "Address to receive notifications",
+      GENDER: {
+        LABEL: "Sex",
+        MALE: "male",
+        FEMALE: "Female"
+      },
+      COMPANY: {
+        NAME: "Company name",
+        ADDRESS: "Company address",
+        MAP: "Company map",
+        LINK_FACEBOOK: "Facebook ID",
+        LINK_ZALO: "Zalo's address",
+        LINK_MESSENGER: "Messenger Address"
+      },
+      SETTINGS: {
+        OTHER: "Other settings",
+        CANCEL: "Cancel",
+        SAVE: "Save",
+        CHANGE: "Change",
+        COMPANY: "Company information"
+      }
     }
   }
-};
+}
--- src/langs/subs/kr.js
+++ src/langs/subs/kr.js
@@ -1,48 +1,272 @@
 export default {
   APP: {
-    LOADING: 'LOADING'
+    LOADING: "로드 중...",
+    BUTTON_BACK: "돌아와",
+    BUTTON_ADD: "새로운 걸 더하다",
+    BUTTON_DELETE_MANY: "많이 삭제",
+    BUTTON_UPDATE: "업데이트",
+    BUTTON_FILTER: "적용하다",
+    TABLE_NO_DATA: "데이터 없음",
+    PLEASE_IMPORT_IMAGE: "사진 선택",
+    REQUIRED_IMAGE_SIZE_4_6: "크기 4x6",
+    DELETE: "삭제",
+    CANCEL: "취소",
+    SAVE: "구하다",
+    UPDATE_STATUS_DONE: "처리 완료",
+    UPDATE_STATUS_IN_PROGRESS: "프로세스 없음",
+    TEXT_PER_PAGE_10: "십",
+    TEXT_PER_PAGE_20: "20",
+    TEXT_PER_PAGE_40: "40",
+    TEXT_PER_PAGE_80: "80",
+    TEXT_PER_PAGE_100: "100",
+    IMPORT_IMAGE: "사진 선택",
+    PLEASE_INPUT: "들어 오세요",
+    PLEASE_SELECT: "선택해 주세요",
+    TEXT_VIEW_MORE: "모두 보기...",
+    PREVIEW: "시사",
+    UPDATE_BANNER: "배너 업데이트",
+    NO_DATA: "데이터 없음",
+    HOT_SALE: "뜨거운 판매"
   },
   TOAST: {
-    SUCCESS: 'SUCCESS',
-    WARNING: 'WARNING',
-    DANGER: 'DANGER',
+    SUCCESS: "성공적인",
+    WARNING: "경고",
+    DANGER: "오류"
+  },
+  LANGUAGES: {
+    LANGUAGES: "언어",
+    VIETNAMESE: "베트남어",
+    ENGLISH: "영어",
+    KOREAN: "한국인"
+  },
+  ROUTER: {
+    COLLABORATORS_MANAGEMENT: "협력자",
+    CAR_MANAGEMENT: "차량 관리",
+    HAPPY_MOMENT: "행복한 순간",
+    CUSTOMER_SUPPORT: "고객 지원",
+    BANNER_MANAGEMENT: "배너 관리",
+    PROFILE: "개인 정보",
+    POLICY_MANAGEMENT: "정책 관리",
+    INSURANCE_MANAGEMENT: "보험 관리"
+  },
+  TOAST_MESSAGE: {
+    CHANGE_LANGUAGE_SUCCESS: "언어를 성공적으로 변경했습니다.",
+    CHANGE_LANGUAGE_ERROR: "언어를 변경하지 못했습니다.",
+    VALIDATE_ACCOUNT_LOGIN: "계정 형식이 올바르지 않습니다. 계정을 다시 확인하십시오",
+    LOGIN_ERROR: "로그인에 실패했습니다. 계정을 다시 확인하십시오.",
+    LOGOUT_ERROR: "로그아웃 실패",
+    CREATE_COLLABORATORS_SUCCESS: "협력자를 성공적으로 생성했습니다.",
+    CREATE_COLLABORATORS_ERROR: "협력자를 생성하지 못했습니다.",
+    EDIT_COLLABORATORS_SUCCESS: "협력자 정보를 성공적으로 편집했습니다.",
+    EDIT_COLLABORATORS_ERROR: "협력자 정보를 편집하지 못했습니다.",
+    DELETE_COLLABORATORS_SUCCESS: "협력자를 성공적으로 삭제했습니다.",
+    DELETE_COLLABORATORS_ERROR: "실패한 공동 작업자를 삭제했습니다.",
+    CREATE_HAPPY_MOMENT_SUCCESS: "재미있는 순간을 성공적으로 만들었습니다.",
+    CREATE_HAPPY_MOMENT_ERROR: "당신은 실패한 행복한 순간을 만들었습니다",
+    EDIT_HAPPY_MOMENT_SUCCESS: "행복한 순간을 성공적으로 편집했습니다.",
+    EDIT_HAPPY_MOMENT_ERROR: "실패한 행복한 순간을 편집했습니다.",
+    DELETE_HAPPY_MOMENT_SUCCESS: "행복한 순간을 성공적으로 지웠다",
+    DELETE_HAPPY_MOMENT_ERROR: "행복했던 실패한 순간을 지웠어",
+    EDIT_CUSTOMER_SUPPORT_SUCCESS: "상태를 성공적으로 업데이트했습니다.",
+    EDIT_CUSTOMER_SUPPORT_ERROR: "상태 업데이트 실패",
+    CREATE_CUSTOMER_SUPPORT_SUCCESS: "지원 연락처를 성공적으로 생성했습니다. 응답을 기다려 주십시오.",
+    CREATE_CUSTOMER_SUPPORT_ERROR: "지원 연락처를 만들지 못했습니다. 다시 시도해 주세요.",
+    UPDATE_BANNER_SUCCESS: "배너를 성공적으로 업데이트했습니다.",
+    UPDATE_BANNER_ERROR: "배너를 업데이트하지 못했습니다.",
+    REQUIRED_FILE: "파일을 선택하세요.",
+    CHANGE_PROFILE_SUCCESS: "당신은 성공적으로 프로필을 업데이트했습니다",
+    CHANGE_PROFILE_ERROR: "프로필을 업데이트하지 못했습니다."
   },
   SHOP_CAR: {
     HOME: {
-      SEARCH: 'SEARCH',
+      SEARCH: "검색",
       CATEGORIES: {
-        DOMESTIC_CAR: 'Domestic car',
-        IMPORTED_CAR: 'Imported car',
-        CUSTOMER_REVIEWS: 'Customer reviews',
-        VEHICLE_LIST: 'Vehicle List'
+        VEHICLE_LIST: "차량 목록",
+        CAR_BUYING_POLICY: "자동차 구매 정책",
+        INSURANCE: "보험",
+        CONTACT: "연락하다"
+      },
+      FILTER: {
+        YEAR: "제조 연도",
+        CATEGORIES: "자동차 회사",
+        COLOR: "색깔",
+        FUEL_TYPE: "연료 종류",
+        GEAR_BOX: "엔진",
+        DISTANCE: "거리",
+        PRICE: "가격"
       },
       SORT_LIST_CAR: {
-        CAR_NAME: 'CAR_NAME',
-        PRICE: 'PRICE',
-        YEAR_MANUFACTURE: 'YEAR_MANUFACTURE',
-
-        TEXT_PER_PAGE_8: 'TEXT_PER_PAGE_8',
-        TEXT_PER_PAGE_16: 'TEXT_PER_PAGE_16',
-        TEXT_PER_PAGE_24: 'TEXT_PER_PAGE_24',
-        TEXT_PER_PAGE_32: 'TEXT_PER_PAGE_32',
-        TEXT_PER_PAGE_40: 'TEXT_PER_PAGE_40',
+        CAR_NAME: "차량 이름",
+        PRICE: "가격",
+        YEAR_MANUFACTURE: "제조 연도",
+        TEXT_PER_PAGE_8: "8차/페이지",
+        TEXT_PER_PAGE_16: "16차/페이지",
+        TEXT_PER_PAGE_24: "24차/페이지",
+        TEXT_PER_PAGE_32: "32대/페이지",
+        TEXT_PER_PAGE_40: "40대/페이지"
       },
       NEW_CAR: {
-        TITLE: 'TITLE',
+        TITLE: "최고의 신차"
       },
       HOT_SALE: {
-        TITLE: 'TITLE',
+        TITLE: "핫세일"
       },
       CUSTOMER_FEEDBACK: {
-        TITLE: 'TITLE'
+        TITLE: "행복한 순간",
+        DETAIL_TITLE: "재미있는 순간 세부 정보"
+      },
+      COLLABORATORS: {
+        TITLE: "협력자"
+      },
+      CONTACT_SUPPORT: {
+        TITLE: "연락처 - Q&A",
+        NAME: "이름",
+        PHONE: "전화 번호",
+        CONTENT: "콘텐츠",
+        SEND: "보내다"
+      },
+      CONTACT: {
+        PHONE: "전화 번호",
+        ADDRESS: "주소",
+        EMAIL: "이메일",
+        MAP: "지도"
+      },
+      PROFILE: {
+        TITLE: "개인 페이지"
+      },
+      LOGOUT: {
+        TITLE: "로그 아웃"
       }
     },
     DETAIL_CAR: {
-      TITLE_BASIC_INFOR: 'TITLE_BASIC_INFOR',
-      YEAR: 'YEAR',
-      FUEL_TYPE: 'FUEL_TYPE',
-      COLOR: 'COLOR',
-      CAR_NUMBER: 'CAR_NUMBER',
+      TITLE: "자세한 차량 정보",
+      TITLE_IMAGE: "이미지 정보",
+      TITLE_BASIC_INFOR: "기본 정보",
+      TITLE_SALLER_INFOR: "판매자 정보",
+      TITLE_VEHICLE_OPTIONS_AND_DETAILS: "차량 옵션 및 세부 정보",
+      TITLE_PERFORMANCE_CHECK: "성능 테스트",
+      PRICE: "가격",
+      LICENSE_PLATE: "번호판",
+      YEAR: "다섯",
+      DISTANCE_DRIVEN: "주행거리",
+      FUEL_TYPE: "연료",
+      TRANSMISSION: "전염",
+      DISPLACEMENT: "이동하다",
+      COLOR: "색깔",
+      CAR_TYPE: "차량 범위",
+      SEIZURE: "경련",
+      MORTGAGE: "저당",
+      PRESENTATION_NUMBER: "발표 번호",
+      STORAGE_LOCATION: "저장소",
+      CONTACT: "연락하다",
+      NAME: "이름",
+      EMPLOYEE_ID: "직원 ID",
+      AFFILIATED_COMPANY: "아래에",
+      BUSINESS_ADDRESS: "사업장 주소",
+      PARKING_LOCATION: "주차 위치",
+      CAR_OPTION_EXTERIOR: "외부",
+      CAR_OPTION_GUTS: "내부",
+      CAR_OPTION_SAFETY: "안전한",
+      CAR_OPTION_CONVENIENCE: "유용"
+    },
+    CAR_BUYING_POLICY: {
+      TITLE: "자동차 구매 정책"
+    },
+    CONTACT: {
+      TITLE: "연락하다"
+    }
+  },
+  DASHBOARD: {
+    LOGIN: {
+      TITLE: "로그인",
+      EMAIL: "이메일",
+      PASSWORD: "비밀번호",
+      LOGIN: "로그인"
+    },
+    COLLABORATORS_MANAGEMENT: {
+      SEARCH: "검색",
+      TABLE: {
+        NO: "STT",
+        AVATAR: "화신",
+        FULLNAME: "이름과 성",
+        TELEPHONE: "전화 번호",
+        EMAIL: "이메일",
+        CONTACT: "연락하다",
+        ACTION: "시장 조작",
+        SNS_KAKAOTALK: "카카오 톡",
+        SNS_ZALO: "잘로",
+        SNS_MESSAGER: "메신저",
+        DESCRIPTION: "설명하다",
+        EDIT: "편집하다",
+        DELETE: "삭제"
+      },
+      MODAL: {
+        TITLE: "협력자 관리"
+      }
+    },
+    HAPPY_MOMENT: {
+      SEARCH: "검색",
+      TABLE: {
+        NO: "STT",
+        PRIMARY_IMAGE: "영상",
+        TITLE: "제목",
+        CONTENT: "콘텐츠",
+        WRITER: "창조자",
+        ACTIONS: "시장 조작",
+        BTN_EDIT: "편집하다",
+        BTN_DELETE: "삭제"
+      },
+      MODAL: {
+        TITLE: "재미있는 순간 관리",
+        LABEL_TITLE: "제목",
+        LABEL_CONTENT: "콘텐츠",
+        LABEL_IMAGES: "그림"
+      }
+    },
+    CUSTOMER_SUPPORT: {
+      TABLE: {
+        NO: "STT",
+        NAME: "이름과 성",
+        PHONE: "전화 번호",
+        QUESTION: "의문",
+        STATUS: "처리 상태",
+        ACTION: "시장 조작",
+        DETAIL: "표시하다",
+        IN_PROGRESS: "프로세스 없음",
+        DONE: "완벽한"
+      }
+    },
+    PROFILE: {
+      INFOR: "기본 정보",
+      CHANGEPASSWORD: "비밀번호 변경",
+      OLDPASSWORDS: "기존 비밀번호",
+      NEWPASSWORDS: "새 비밀번호",
+      FULLNAME: "이름과 성",
+      DOB: "생일",
+      EMAIL: "이메일 주소",
+      PHONE: "전화 번호",
+      ADDRESS: "주소",
+      ADDRESS_MAIL_NOTI: "알림을 받을 주소",
+      GENDER: {
+        LABEL: "섹스",
+        MALE: "남성",
+        FEMALE: "여성"
+      },
+      COMPANY: {
+        NAME: "회사 이름",
+        ADDRESS: "회사 주소",
+        MAP: "회사 지도",
+        LINK_FACEBOOK: "페이스 북 아이디",
+        LINK_ZALO: "잘로의 주소",
+        LINK_MESSENGER: "메신저 주소"
+      },
+      SETTINGS: {
+        OTHER: "다른 설정",
+        CANCEL: "취소",
+        SAVE: "구하다",
+        CHANGE: "변화",
+        COMPANY: "회사 정보"
+      }
     }
   }
-};
+}
