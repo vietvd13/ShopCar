b1bc26ab1dd9567a2955cc71ec9d3ac14b28cc8d
Duck <vuducviet0131@gmail.com>
2022-11-24 22:21:05 +0700

    Add footer

--- src/views/ShopCar/components/FooterHome.vue
+++ src/views/ShopCar/components/FooterHome.vue
@@ -1,68 +1,92 @@
 <template>
     <div class="footer-home">
-        <div class="content-lang">
-            <div class="main-content">
-                <b-col>
-                    <b-row>
-                        <b-col>
-                            <div
-                                :class="{'item-lang': true, 'active-lang': lang === CONSTANTS.VALUE.LANGUAGE_VIETNAMESE}"
-                                @click="setLanguage(CONSTANTS.VALUE.LANGUAGE_VIETNAMESE)"
-                            >
-                                {{ $t('LANGUAGES.VIETNAMESE') }}
-                            </div>
-                        </b-col>
-                        <b-col>
-                            <div
-                                :class="{'item-lang': true, 'active-lang': lang === CONSTANTS.VALUE.LANGUAGE_ENGLISH}"
-                                @click="setLanguage(CONSTANTS.VALUE.LANGUAGE_ENGLISH)"
-                            >
-                                {{ $t('LANGUAGES.ENGLISH') }}
-                            </div>
-                        </b-col>
-                        <b-col>
-                            <div
-                                :class="{'item-lang': true, 'active-lang': lang === CONSTANTS.VALUE.LANGUAGE_KOREAN}"
-                                @click="setLanguage(CONSTANTS.VALUE.LANGUAGE_KOREAN)"
-                            >
-                                {{ $t('LANGUAGES.KOREAN') }}
-                            </div>
-                        </b-col>
-                    </b-row>
+        <b-container>
+            <b-row>
+                <b-col cols="12" xs="12" sm="12" md="12" lg="6" xl="6">
+                    <div class="footer-home__company-name">
+                        {{ profile.company_name }}
+                    </div>
+
+                    <div class="footer-home__address">
+                        <b>{{ $t('SHOP_CAR.HOME.CONTACT.ADDRESS') }}:</b> 
+                        {{ profile.company_address }}
+                    </div>
+
+                    <div class="footer-home__website">
+                        <b>{{ $t('SHOP_CAR.HOME.CONTACT.WEBSITE') }}:</b> 
+                        {{ domain }}
+                    </div>
+
+                    <div class="footer-home__email">
+                        <b>{{ $t('SHOP_CAR.HOME.CONTACT.EMAIL') }}:</b> 
+                        {{ profile.email }}
+                    </div>
+
+                    <div class="footer-home__phone-number">
+                        <b>{{ $t('SHOP_CAR.HOME.CONTACT.PHONE') }}:</b> 
+                        {{ profile.phone_number }}
+                    </div>
                 </b-col>
-            </div>
-        </div>
+
+                <b-col cols="12" xs="12" sm="12" md="12" lg="6" xl="6">
+                    <iframe
+                        :src="profile.map || `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.904149296542!2d105.81368901533224!3d21.036520892888273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab6d7fee2771%3A0xc4a199ae8b20e4c7!2zMjY2IMSQ4buZaSBD4bqlbg!5e0!3m2!1svi!2s!4v1669199823487!5m2!1svi!2s`"
+                        width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade" />
+                </b-col>
+            </b-row>
+        </b-container>
     </div>
 </template>
 
 <script>
-import CONSTANTS from '@/constants';
-import Toast from '@/toast';
+import { getProfilePublic } from '@/api/modules/Home';
+import { setLoading } from '@/utils/setLoading';
 
 export default {
     name: 'FooterHome',
     computed: {
-        lang() {
-            return this.$store.getters.language; 
+        domain() {
+            return window.origin;
         }
     },
     data() {
         return {
-            CONSTANTS
+            profile: {
+                phone_number: '',
+                company_address: '',
+                email: '',
+                map: '',
+                company_name: '',
+            }
         }
     },
+    created() {
+        this.initData();
+    },
     methods: {
-        setLanguage(language = CONSTANTS.VALUE.LANGUAGE_KOREAN) {
-            if (this.lang !== language) {
-                this.$store.dispatch('app/setLanguage', language)
-                    .then(() => {
-                        this.$i18n.locale = language;
+        async initData() {
+            await this.handleGetProfile();
+        },
+        async handleGetProfile() {
+            try {
+                setLoading(true);
 
-                        Toast.success(this.$t('TOAST_MESSAGE.CHANGE_LANGUAGE_SUCCESS'));
-                    })
-                    .catch(() => {
-                        Toast.warning(this.$t('TOAST_MESSAGE.CHANGE_LANGUAGE_ERROR'))
-                    })
+                const { status_code, data } = await getProfilePublic();
+
+                if (status_code === 200) {
+                    this.profile.phone_number = data.phone_number;
+                    this.profile.company_address = data.company_address;
+                    this.profile.email = data.email;
+                    this.profile.map = data.company_map;
+                    this.profile.company_name = data.company_name;
+
+                }
+
+                setLoading(false);
+            } catch (err) {
+                setLoading(false);
+                console.log(err);
             }
         }
     },
@@ -73,23 +97,22 @@ export default {
 @import '@/scss/variables';
 
 .footer-home {
-    width: 100%;
     background-color: $mine-shaft;
     color: $white;
-    padding: 5px;
 
-    .content-lang {
-        .item-lang {
-            padding: 5px 0;
+    padding: 20px 0;
 
-            text-align: center;
-            font-weight: 600;
-            cursor: pointer;
-        }
+    &__company-name {
+        font-size: 22px;
+        margin-bottom: 20px;
+    }
 
-        .active-lang {
-            color: $white;
-        }
+    &__address,
+    &__website,
+    &__email,
+    &__phone-number {
+        font-size: 15px;
+        margin-bottom: 10px;
     }
 }
 </style>
