28eb8d6259126bd2975a6c570fe144f0afec2734
Duck <vuducviet0131@gmail.com>
2022-11-28 00:12:03 +0700

    Fix position

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -68,23 +68,25 @@ function formatNumber(number) {
 }
 
 function elementInViewport(el) {
-    var rect     = el.getBoundingClientRect(),
+    if (el) {
+        var rect     = el.getBoundingClientRect(),
         vWidth   = window.innerWidth || document.documentElement.clientWidth,
         vHeight  = window.innerHeight || document.documentElement.clientHeight,
         efp      = function (x, y) { return document.elementFromPoint(x, y) };     
 
-    // Return false if it's not in the viewport
-    if (rect.right < 0 || rect.bottom < 0 
-            || rect.left > vWidth || rect.top > vHeight)
-        return false;
+        // Return false if it's not in the viewport
+        if (rect.right < 0 || rect.bottom < 0 
+                || rect.left > vWidth || rect.top > vHeight)
+            return false;
 
-    // Return true if any of its four corners are visible
-    return (
-        el.contains(efp(rect.left,  rect.top))
-        ||  el.contains(efp(rect.right, rect.top))
-        ||  el.contains(efp(rect.right, rect.bottom))
-        ||  el.contains(efp(rect.left,  rect.bottom))
-    );
+        // Return true if any of its four corners are visible
+        return (
+            el.contains(efp(rect.left,  rect.top))
+            ||  el.contains(efp(rect.right, rect.top))
+            ||  el.contains(efp(rect.right, rect.bottom))
+            ||  el.contains(efp(rect.left,  rect.bottom))
+        );
+    }
 }
 
 function handleSrollTop() {
